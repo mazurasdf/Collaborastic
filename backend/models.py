d65709fc3902fd10036f751d1f257991b8e8478f
Michael Mazur <mazurasdf@gmail.com>
2020-03-21 18:58:04 -0500

    first actual migration. needs testing...

--- backend/models.py
+++ backend/models.py
@@ -10,23 +10,23 @@ class Skill(models.Model):
 	# users
 
 class User(models.Model):
-    email = models.CharField(max_length=320)
-    password = models.CharField(max_length=60)
+	email = models.CharField(max_length=320)
+	password = models.CharField(max_length=60)
 	first_name = models.CharField(max_length=25)
-    last_name = models.CharField(max_length=25)
+	last_name = models.CharField(max_length=25)
 	expertise = models.IntegerField()
 	experience = models.CharField(max_length=50)
 	location = models.CharField(max_length=50)
 	bio = models.TextField()
-	connections = models.ManyToManyField("self", blank=true)
-	skills = models.ForeignKey(Skill, related_name="users")
+	connections = models.ManyToManyField("self", blank=True, symmetrical=True)
+	skills = models.ForeignKey(Skill, related_name="users", on_delete=models.CASCADE)
 	# events
 	# collaborations
 	# attending
 	# event_jobs
 	created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    # objects = User_Manager()
+	updated_at = models.DateTimeField(auto_now=True)
+	# objects = User_Manager()
 
 class Event(models.Model):
 	creator = models.ForeignKey(User, related_name='events', on_delete=models.CASCADE)
@@ -37,17 +37,17 @@ class Event(models.Model):
 	attendees = models.ManyToManyField(User, related_name='attending')
 	tags = models.ManyToManyField(Tag, related_name="events")
 	created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+	updated_at = models.DateTimeField(auto_now=True)
 
 class Event_Job(models.Model):
 	creator = models.ForeignKey(User, related_name='event_jobs', on_delete=models.CASCADE)
 	description = models.TextField()
 	struggle = models.CharField(max_length=50)
-	date_decided = models.IntegerField(min_value=0, max_value=2) #is date undecided(0), final(1), or flexible(2)
+	date_decided = models.IntegerField() #is date undecided(0), final(1), or flexible(2)
 	scope_date = models.DateTimeField()
 	pay_hour = models.DecimalField(max_digits=6, decimal_places=2)
 	pay_total = models.DecimalField(max_digits=6, decimal_places=2)
-	event_place = models.IntegerField(min_value=0, max_value=3)
+	event_place = models.IntegerField()
 	# event place is no preference(0),in person(1), online(2), both(3)
 	tags = models.ManyToManyField(Tag, related_name="event_jobs")
 	partner_description = models.TextField()
